3d2d707f13226792643658f3e77a02e13c34ea7b
unknown <victorchaparro615@gmail.com>
2021-10-14 22:23:19 -0500

    corregir sintaxis

--- TorneoDeFutbol.App/TorneoDeFutbol.App.Dominio/Entidades/Participante.cs
+++ TorneoDeFutbol.App/TorneoDeFutbol.App.Dominio/Entidades/Participante.cs
@@ -5,26 +5,26 @@ namespace TorneoDeFutbol.App.Dominio
 {
     public class Participante
     {
-    [Key]
-     public int idParticipante { get;set; }
-     [Display(Name = "Número de Identificación Personal")]
-     [StringLength(10)]
-     public string numDocumento { get;set; }
-     [Display(Name = "Nombres")]
-     public string nombre { get;set; }
-     [Display(Name = "Apellidos")]
-     public string apellido { get;set; }     
-     [Display(Name = "Dirección")]
-     public string direccion { get;set; }
-     [Display(Name = "Número de Teléfono")]
-     [StringLength(10)]
-     public string numTelefono  { get;set; }
-     [Display(Name = "Ciudad")]
-     public string ciudad  { get;set; }
-     [Display(Name = "Género")]
-     public Genero genero { get;set; }
-     [Display(Name = "Fecha de nacimiento")]
-     public DateTime fechaNacimiento { get;set; }
+        [Key]
+        public int idParticipante { get;set; }
+        [Display(Name = "Número de Identificación Personal")]
+        [StringLength(10)]
+        public string numDocumento { get;set; }
+        [Display(Name = "Nombres")]
+        public string nombre { get;set; }
+        [Display(Name = "Apellidos")]
+        public string apellido { get;set; }     
+        [Display(Name = "Dirección")]
+        public string direccion { get;set; }
+        [Display(Name = "Número de Teléfono")]
+        [StringLength(10)]
+        public string numTelefono  { get;set; }
+        [Display(Name = "Ciudad")]
+        public string ciudad  { get;set; }
+        [Display(Name = "Género")]
+        public Genero genero { get;set; }
+        [Display(Name = "Fecha de nacimiento")]
+        public DateTime fechaNacimiento { get;set; }
    
     }
 }

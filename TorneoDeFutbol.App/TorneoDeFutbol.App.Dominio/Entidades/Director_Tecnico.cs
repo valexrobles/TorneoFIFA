0b57520ec955259c3e875d697be5d6d58a929977
Richard <richbles444@gmail.com>
2021-10-15 22:06:53 -0500

    22 06 PM Octubre 15

--- TorneoDeFutbol.App/TorneoDeFutbol.App.Dominio/Entidades/Director_Tecnico.cs
+++ TorneoDeFutbol.App/TorneoDeFutbol.App.Dominio/Entidades/Director_Tecnico.cs
@@ -6,6 +6,6 @@ namespace TorneoDeFutbol.App.Dominio
     public class Director_Tecnico: Participante
     {
         [Display(Name = "Años de Experiencia")]
-        public int aniosExperiencia { get;set;}
+        public string aniosExperiencia { get;set;}
     }
 }
